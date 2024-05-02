d2f40a204685804d2f099e7cbf1265ca181f94cd
shahin-hossain-dev <shahin.hossain.dev@gmail.com>
2024-05-02 10:35:42 +0600

    59-4 Extract Booking Service Data

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,18 +1,42 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Checkout = () => {
+  const { user } = useContext(AuthContext);
   const service = useLoaderData();
-  console.log(service);
+  const { title, _id, price } = service;
+
+  const handleBookService = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = form.email.value;
+
+    const order = {
+      customerName: name,
+      service: _id,
+      email,
+      date,
+      price,
+    };
+
+    console.log(order);
+  };
   return (
     <div>
-      <form className="card-body">
+      <h2 className="text-3xl text-center">Books Services: {title}</h2>
+      <form onSubmit={handleBookService} className="card-body">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="form-control">
             <label className="label">
-              <span className="label-text">First Name</span>
+              <span className="label-text">Full Name</span>
             </label>
             <input
               type="text"
+              name="name"
+              defaultValue={user?.displayName}
               placeholder="Name"
               className="input input-bordered"
               required
@@ -20,41 +44,49 @@ const Checkout = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Last Name</span>
+              <span className="label-text">Date</span>
+            </label>
+            <input
+              type="date"
+              name="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              defaultValue={user?.email}
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Due Amount</span>
             </label>
             <input
               type="text"
-              placeholder="password"
+              name=""
+              placeholder="due ammount"
               className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
+              defaultValue={"$ " + price}
               required
             />
           </div>
         </div>
 
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Order Confirm</button>
+          <input
+            type="submit"
+            className="btn btn-warning"
+            value="Order Confirm"
+          />
         </div>
       </form>
     </div>
